7d4ca22dc74911afb8513d023222c8ede38d82e4
Richard Shin <shinrichard.1992@gmail.com>
2021-08-01 03:55:05 -0400

    Changes to design of home page.

--- homePage.bundle.js
+++ homePage.bundle.js
@@ -16,14 +16,45 @@
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ homePage)\n/* harmony export */ });\nfunction homePage() {\n    const bodyCont = document.createElement('div');\n    const leftCont = document.createElement('div');\n    const restName = document.createElement('span');\n    const tabCont = document.createElement('div');\n    const homeItem = document.createElement('span');\n    const menuItem = document.createElement('span');\n    const contactItem = document.createElement('span');\n    const mainCont = document.createElement('div');\n    const imgCont = document.createElement('div');\n    const homeImg = document.createElement('img');\n    const infoCont = document.createElement('div');\n\n    bodyCont.appendChild(leftCont);\n    leftCont.appendChild(restName);\n    leftCont.appendChild(tabCont);\n    tabCont.appendChild(homeItem);\n    tabCont.appendChild(menuItem);\n    tabCont.appendChild(contactItem);\n    bodyCont.appendChild(mainCont);\n    mainCont.appendChild(imgCont);\n    imgCont.appendChild(homeImg);\n    mainCont.appendChild(infoCont);\n\n    restName.innerHTML = 'Seventh Heaven Caf&egrave;';\n    homeItem.innerHTML = 'Home';\n    menuItem.innerHTML = 'Menu';\n    contactItem.innerHTML = 'Contact';\n\n    leftCont.setAttribute('id', 'left-cont');\n    restName.setAttribute('id', 'rest-name');\n    tabCont.setAttribute('id', 'tab-cont');\n    homeItem.classList.add('menu-items');\n    menuItem.classList.add('menu-items');\n    contactItem.classList.add('menu-items');\n    mainCont.setAttribute('id', 'main-cont');\n    mainCont.classList.add('parallax');\n    imgCont.setAttribute('id', 'img-cont');\n    imgCont.classList.add('parallax-layer', 'parallax-layer-back');\n    homeImg.src = '../src/images/cafe-main-7.jpeg';\n    infoCont.setAttribute('id', 'info-cont');\n    infoCont.classList.add('parallax-layer', 'parallax-layer-base');\n    bodyCont.setAttribute('id', 'body-cont');\n\n    return bodyCont;\n}\n\n//# sourceURL=webpack://restaurant-page/./src/homePage.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ homePage)\n/* harmony export */ });\n/* harmony import */ var _images_cafe_main_8_jpeg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./images/cafe-main-8.jpeg */ \"./src/images/cafe-main-8.jpeg\");\n\n\nfunction homePage() {\n    const bodyCont = document.createElement('div');\n    const leftCont = document.createElement('div');\n    const restName = document.createElement('span');\n    const tabCont = document.createElement('div');\n    const homeItem = document.createElement('span');\n    const menuItem = document.createElement('span');\n    const contactItem = document.createElement('span');\n    const mainCont = document.createElement('div');\n    const imgCont = document.createElement('div');\n    const homeImg = document.createElement('img');\n    const infoCont = document.createElement('div');\n\n    bodyCont.appendChild(leftCont);\n    leftCont.appendChild(restName);\n    leftCont.appendChild(tabCont);\n    tabCont.appendChild(homeItem);\n    tabCont.appendChild(menuItem);\n    tabCont.appendChild(contactItem);\n    bodyCont.appendChild(mainCont);\n    mainCont.appendChild(imgCont);\n    imgCont.appendChild(homeImg);\n    mainCont.appendChild(infoCont);\n\n    restName.innerHTML = 'Seventh Heaven Caf&egrave;';\n    homeItem.innerHTML = 'Home';\n    menuItem.innerHTML = 'Menu';\n    contactItem.innerHTML = 'Contact';\n\n    leftCont.setAttribute('id', 'left-cont');\n    restName.setAttribute('id', 'rest-name');\n    tabCont.setAttribute('id', 'tab-cont');\n    homeItem.classList.add('menu-items');\n    menuItem.classList.add('menu-items');\n    contactItem.classList.add('menu-items');\n    mainCont.setAttribute('id', 'main-cont');\n    mainCont.classList.add('parallax');\n    imgCont.setAttribute('id', 'img-cont');\n    imgCont.classList.add('parallax-layer', 'parallax-layer-back');\n    homeImg.src = _images_cafe_main_8_jpeg__WEBPACK_IMPORTED_MODULE_0__;\n    infoCont.setAttribute('id', 'info-cont');\n    infoCont.classList.add('parallax-layer', 'parallax-layer-base');\n    bodyCont.setAttribute('id', 'body-cont');\n\n    return bodyCont;\n}\n\n//# sourceURL=webpack://restaurant-page/./src/homePage.js?");
+
+/***/ }),
+
+/***/ "./src/images/cafe-main-8.jpeg":
+/*!*************************************!*\
+  !*** ./src/images/cafe-main-8.jpeg ***!
+  \*************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"accea90ffc81728d706e.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/cafe-main-8.jpeg?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -36,6 +67,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 				}
 /******/ 			}
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -54,13 +97,32 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/homePage.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/homePage.js");
 /******/ 	
 /******/ })()
 ;
--- menuPage.bundle.js
+++ menuPage.bundle.js
@@ -16,7 +16,7 @@
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\nconst menuPage = () => {\n\n}\n\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (menuPage);\n\n//# sourceURL=webpack://restaurant-page/./src/menuPage.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ menuPage)\n/* harmony export */ });\nfunction menuPage() {\n\n}\n\n//# sourceURL=webpack://restaurant-page/./src/menuPage.js?");
 
 /***/ })
 
