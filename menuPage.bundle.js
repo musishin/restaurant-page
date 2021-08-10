6fd5c5de4d9d0ddbd638328f65e60141b1ad775d
Richard Shin <shinrichard.1992@gmail.com>
2021-08-10 05:21:11 -0400

    Menu page is nearly complete.

--- menuPage.bundle.js
+++ menuPage.bundle.js
@@ -10,20 +10,111 @@
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
+/***/ "./src/images/cafe-img-2.jpeg":
+/*!************************************!*\
+  !*** ./src/images/cafe-img-2.jpeg ***!
+  \************************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"99d0882378f34325c209.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/cafe-img-2.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food1.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food1.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"7374600e6aec1b07f04b.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food1.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food2.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food2.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"bca947ce855616602849.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food2.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food3.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food3.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"b0df87e89051c78b29ec.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food3.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food4.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food4.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"a2680667e7db72e012d8.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food4.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food5.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food5.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"b4180cb2978b3a41b7c7.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food5.jpeg?");
+
+/***/ }),
+
+/***/ "./src/images/food6.jpeg":
+/*!*******************************!*\
+  !*** ./src/images/food6.jpeg ***!
+  \*******************************/
+/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+
+eval("module.exports = __webpack_require__.p + \"052001beb29ab679e015.jpeg\";\n\n//# sourceURL=webpack://restaurant-page/./src/images/food6.jpeg?");
+
+/***/ }),
+
 /***/ "./src/menuPage.js":
 /*!*************************!*\
   !*** ./src/menuPage.js ***!
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ menuPage)\n/* harmony export */ });\nfunction menuPage() {\n    const bodyCont = document.createElement('div');\n    const menuTitle = document.createElement('menu-title');\n    const menuCont = document.createElement('div');\n\n    bodyCont.appendChild(menuTitle);\n    bodyCont.appendChild(menuCont);\n\n    menuTitle.innerHTML = 'Browse Our Menu';\n\n    bodyCont.classList.add('body-cont');\n    bodyCont.setAttribute('id', 'menu-body-cont');\n    menuTitle.setAttribute('id', 'menu-title');\n    menuCont.setAttribute('id', 'menu-cont');\n\n    return bodyCont;\n}\n\n//# sourceURL=webpack://restaurant-page/./src/menuPage.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ menuPage)\n/* harmony export */ });\n/* harmony import */ var _images_food1_jpeg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./images/food1.jpeg */ \"./src/images/food1.jpeg\");\n/* harmony import */ var _images_food2_jpeg__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./images/food2.jpeg */ \"./src/images/food2.jpeg\");\n/* harmony import */ var _images_food3_jpeg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./images/food3.jpeg */ \"./src/images/food3.jpeg\");\n/* harmony import */ var _images_food4_jpeg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./images/food4.jpeg */ \"./src/images/food4.jpeg\");\n/* harmony import */ var _images_food5_jpeg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./images/food5.jpeg */ \"./src/images/food5.jpeg\");\n/* harmony import */ var _images_food6_jpeg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./images/food6.jpeg */ \"./src/images/food6.jpeg\");\n/* harmony import */ var _images_cafe_img_2_jpeg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ./images/cafe-img-2.jpeg */ \"./src/images/cafe-img-2.jpeg\");\n\n\n\n\n\n\n\n\nfunction menuPage() {\n    const bodyCont = document.createElement('div');\n    const menuImgCont = document.createElement('div');\n    const menuImg = document.createElement('img');\n    const menuTitle = document.createElement('div');\n    const menuCont = document.createElement('div');\n    const foodItem1 = document.createElement('div');\n    const foodItem2 = document.createElement('div');\n    const foodItem3 = document.createElement('div');\n    const foodItem4 = document.createElement('div');\n    const foodItem5 = document.createElement('div');\n    const foodItem6 = document.createElement('div');\n    const foodImg1 = document.createElement('img');\n    const foodImg2 = document.createElement('img');\n    const foodImg3 = document.createElement('img');\n    const foodImg4 = document.createElement('img');\n    const foodImg5 = document.createElement('img');\n    const foodImg6 = document.createElement('img');\n    const food1Txt = document.createElement('div');\n    const food2Txt = document.createElement('div');\n    const food3Txt = document.createElement('div');\n    const food4Txt = document.createElement('div');\n    const food5Txt = document.createElement('div');\n    const food6Txt = document.createElement('div');\n    const food1Price = document.createElement('div');\n    const food2Price = document.createElement('div');\n    const food3Price = document.createElement('div');\n    const food4Price = document.createElement('div');\n    const food5Price = document.createElement('div');\n    const food6Price = document.createElement('div');\n\n    bodyCont.appendChild(menuImgCont);\n    menuImgCont.appendChild(menuImg);\n    menuImgCont.appendChild(menuTitle);\n    bodyCont.appendChild(menuCont);\n    menuCont.appendChild(foodItem1);\n    menuCont.appendChild(foodItem2);\n    menuCont.appendChild(foodItem3);\n    menuCont.appendChild(foodItem4);\n    menuCont.appendChild(foodItem5);\n    menuCont.appendChild(foodItem6);\n    foodItem1.appendChild(foodImg1);\n    foodItem2.appendChild(foodImg2);\n    foodItem3.appendChild(foodImg3);\n    foodItem4.appendChild(foodImg4);\n    foodItem5.appendChild(foodImg5);\n    foodItem6.appendChild(foodImg6);\n    foodItem1.appendChild(food1Txt);\n    foodItem1.appendChild(food1Price);\n    foodItem2.appendChild(food2Txt);\n    foodItem2.appendChild(food2Price);\n    foodItem3.appendChild(food3Txt);\n    foodItem3.appendChild(food3Price);\n    foodItem4.appendChild(food4Txt);\n    foodItem4.appendChild(food4Price);\n    foodItem5.appendChild(food5Txt);\n    foodItem5.appendChild(food5Price);\n    foodItem6.appendChild(food6Txt);\n    foodItem6.appendChild(food6Price);\n\n    menuTitle.innerHTML = 'Pastry';\n    food1Txt.innerHTML = 'Taro Buttercream Bread';\n    food1Price.innerHTML = '$12.95';\n    food2Txt.innerHTML = 'Choco Cream Bread';\n    food2Price.innerHTML = '$12.95';\n    food3Txt.innerHTML = 'Honey Cheese Mochi Pancake';\n    food3Price.innerHTML = '$12.95';\n    food4Txt.innerHTML = 'Cream Cheese Stick Bread';\n    food4Price.innerHTML = '$12.95';\n    food5Txt.innerHTML = 'Garlic Cream Cheese Bread';\n    food5Price.innerHTML = '$12.95';\n    food6Txt.innerHTML = 'Red Bean Bread';\n    food6Price.innerHTML = '$12.95';\n\n    menuImg.src = _images_cafe_img_2_jpeg__WEBPACK_IMPORTED_MODULE_6__;\n    foodImg1.src = _images_food1_jpeg__WEBPACK_IMPORTED_MODULE_0__;\n    foodImg2.src = _images_food2_jpeg__WEBPACK_IMPORTED_MODULE_1__;\n    foodImg3.src = _images_food3_jpeg__WEBPACK_IMPORTED_MODULE_2__;\n    foodImg4.src = _images_food4_jpeg__WEBPACK_IMPORTED_MODULE_3__;\n    foodImg5.src = _images_food5_jpeg__WEBPACK_IMPORTED_MODULE_4__;\n    foodImg6.src = _images_food6_jpeg__WEBPACK_IMPORTED_MODULE_5__;\n\n    bodyCont.setAttribute('id', 'menu-body-cont');\n    menuImgCont.setAttribute('id', 'menu-top-cont');\n    menuImg.setAttribute('id', 'menu-top-img');\n    menuTitle.setAttribute('id', 'menu-title');\n    menuCont.setAttribute('id', 'menu-cont');\n    foodItem1.classList.add('food-item');\n    foodItem2.classList.add('food-item');\n    foodItem3.classList.add('food-item');\n    foodItem4.classList.add('food-item');\n    foodItem5.classList.add('food-item');\n    foodItem6.classList.add('food-item');\n    foodImg1.classList.add('food-img');\n    foodImg2.classList.add('food-img');\n    foodImg3.classList.add('food-img');\n    foodImg4.classList.add('food-img');\n    foodImg5.classList.add('food-img');\n    foodImg6.classList.add('food-img');\n    food1Txt.classList.add('food-txt');\n    food1Price.classList.add('food-price');\n    food2Txt.classList.add('food-txt');\n    food2Price.classList.add('food-price');\n    food3Txt.classList.add('food-txt');\n    food3Price.classList.add('food-price');\n    food4Txt.classList.add('food-txt');\n    food4Price.classList.add('food-price');\n    food5Txt.classList.add('food-txt');\n    food5Price.classList.add('food-price');\n    food6Txt.classList.add('food-txt');\n    food6Price.classList.add('food-price');\n\n    return bodyCont;\n}\n\n//# sourceURL=webpack://restaurant-page/./src/menuPage.js?");
 
 /***/ })
 
 /******/ 	});
 /************************************************************************/
-/******/ 	// The require scope
-/******/ 	var __webpack_require__ = {};
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
 /******/ 	
 /************************************************************************/
 /******/ 	/* webpack/runtime/define property getters */
@@ -36,6 +127,18 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 				}
 /******/ 			}
 /******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/global */
+/******/ 	(() => {
+/******/ 		__webpack_require__.g = (function() {
+/******/ 			if (typeof globalThis === 'object') return globalThis;
+/******/ 			try {
+/******/ 				return this || new Function('return this')();
+/******/ 			} catch (e) {
+/******/ 				if (typeof window === 'object') return window;
+/******/ 			}
+/******/ 		})();
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
@@ -54,13 +157,32 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/publicPath */
+/******/ 	(() => {
+/******/ 		var scriptUrl;
+/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
+/******/ 		var document = __webpack_require__.g.document;
+/******/ 		if (!scriptUrl && document) {
+/******/ 			if (document.currentScript)
+/******/ 				scriptUrl = document.currentScript.src
+/******/ 			if (!scriptUrl) {
+/******/ 				var scripts = document.getElementsByTagName("script");
+/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
+/******/ 			}
+/******/ 		}
+/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
+/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
+/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
+/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
+/******/ 		__webpack_require__.p = scriptUrl;
+/******/ 	})();
+/******/ 	
 /************************************************************************/
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
 /******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = {};
-/******/ 	__webpack_modules__["./src/menuPage.js"](0, __webpack_exports__, __webpack_require__);
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/menuPage.js");
 /******/ 	
 /******/ })()
 ;
